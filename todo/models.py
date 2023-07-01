b1c4d1445743572ad8dc27845262c878ecb46644
SaeedTSR <saeedroostaei79@gmail.com>
2023-07-01 14:44:14 +0330

    Configs

--- todo/models.py
+++ todo/models.py
@@ -1,12 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-import datetime
 
 user = get_user_model()
-
-x = datetime.datetime.today().strftime('%Y-%m-%d')
-now = datetime.datetime.strptime(x, "%Y-%m-%d")
-later = now + datetime.timedelta(days=10)
 
 class Task(models.Model):
     user = models.ForeignKey(user, on_delete=models.CASCADE)
@@ -18,7 +13,7 @@ class Task(models.Model):
     status = models.CharField(
         max_length=7, choices=STATUS_CHOICES, default='Not yet')
     created_date = models.DateTimeField(auto_now_add=True)
-    due_date = models.DateField(null=True, default=later)
+    due_date = models.DateField(null=True, help_text='yyyy-mm-dd')
 
     def __str__(self):
         return self.name
